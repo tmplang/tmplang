d4016e21f282fb61cd899b4dfc045baeede5ae74
Robert Esclapez Garcia <robertesclapez@gmail.com>
2022-10-20 19:37:13 +0200

    [PAR] Create Source Tree from parsing

--- tmplang/include/tmplang/Parser/Parser.h
+++ tmplang/include/tmplang/Parser/Parser.h
@@ -1,15 +1,15 @@
 #ifndef TMPLANG_PARSER_PARSER_H
 #define TMPLANG_PARSER_PARSER_H
 
+#include <llvm/ADT/Optional.h>
 #include <tmplang/Lexer/Lexer.h>
+#include <tmplang/Tree/Source/CompilationUnit.h>
 
 namespace tmplang {
 
-class HIRContext;
-
-/// Simple grammar verifier parser. Given a lexer which already contains the code
-/// returns wether the grammar can generate the input
-bool Parse(tmplang::Lexer &lex);
+/// Simple grammar verifier parser. Given a lexer which already contains the
+/// code returns wether the grammar can generate the input
+llvm::Optional<source::CompilationUnit> Parse(tmplang::Lexer &lex);
 
 } // namespace tmplang
 
