bce274c118272f2d18da3aa526371fbf48c003f0
Robert Esclapez Garcia <robertesclapez@gmail.com>
2022-10-23 19:42:19 +0200

    [HIR] Move HIR unittests to FileCheck tests

--- tmplang/tests/unittests/HIR/HIRTest.cpp
+++ tmplang/tests/unittests/HIR/HIRTest.cpp
@@ -5,29 +5,6 @@
 
 using namespace tmplang;
 
-TEST(HIRTest, Valid) {
-  // Only i32, is valid right now
-  llvm::StringLiteral tests[] = {"",
-                                 "proc foo: i32 a -> i32 {}",
-                                 "proc foo: i32 a, i32 b -> i32 {}",
-                                 "proc foo: i32 a, i32 b {}",
-                                 "proc foo -> i32 {}",
-                                 "proc foo {}",
-                                 "fn foo: i32 a -> i32 {}",
-                                 "fn foo: i32 a, i32 b -> i32 {}",
-                                 "fn foo -> i32 {}",
-                                 "fn foo {}"};
-
-  for (const llvm::StringLiteral &code : tests) {
-    Lexer lex(code);
-    auto srcCompUnit = Parse(lex);
-    ASSERT_TRUE(srcCompUnit);
-    hir::HIRContext ctx;
-    auto hirCompUnit = hir::buildHIR(*srcCompUnit, ctx);
-    EXPECT_TRUE(hirCompUnit);
-  }
-}
-
 TEST(HIRTest, Invalid) {
   // Neither "Type", nor "void" are valid types
   llvm::StringLiteral tests[] = {
